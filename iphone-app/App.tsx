89bf42e31ec0cf961ea863c2630ecc8f781ee20d
David Chis <chisdavid4321@gmail.com>
2026-03-16 15:26:30 +0200

    Fix icons

--- iphone-app/App.tsx
+++ iphone-app/App.tsx
@@ -1,11 +1,19 @@
+import { Ionicons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
 import { scheduleDailyEncouragementNotifications } from './src/utils/notificationScheduler';
 
 const App = () => {
+  const [fontsLoaded] = useFonts(Ionicons.font);
+
   useEffect(() => {
     void scheduleDailyEncouragementNotifications();
   }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return <RootNavigator />;
 };
